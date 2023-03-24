070f4f1fb1bea80e2bd8d371893cf1a165610172
Elina <vetox101@gmail.com>
2023-03-24 21:13:05 +0300

    init

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -34,24 +34,4 @@ public class CardDeliveryTest {
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
                         Duration.ofSeconds(15));
     }
-    @Test   //№2
-    public void testCards() {
-        $("[data-test-id=city] input").setValue("Ан");
-        $(Selectors.byText("Москва")).click();
-        $("[data-test-id=date] input").click();
-        int days = 4;
-        for ( int cycle = 0; cycle < days; cycle++) {
-            $(".calendar").sendKeys(Keys.ARROW_RIGHT);
-        }
-        $(".calendar").sendKeys(Keys.ENTER);
-        $("[data-test-id=name] input").setValue("Жан-Поль Иванов");
-        $("[data-test-id=phone] input").setValue("+12345678901");
-        $("[data-test-id=agreement]").click();
-        $(".button").shouldHave(Condition.text("Забронировать")).click();
-        String verificationDate = LocalDate.now().plusDays(7)
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=notification]")
-                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
-                        Duration.ofSeconds(15));
-    }
 }
